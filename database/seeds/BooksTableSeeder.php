c2e03c0fd4a21269af91890ac232f5e692196e69
Alessandro Liggieri <smmb@live.it>
2021-03-26 17:04:04 +0100

    Esercizio completo

--- database/seeds/BooksTableSeeder.php
+++ database/seeds/BooksTableSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use App\Book;
+use Faker\Generator as Faker;
+
 class BooksTableSeeder extends Seeder
 {
     /**
@@ -9,8 +11,16 @@ class BooksTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $book = new Book();
+            $book->title = $faker->text(20);
+            $book->author = $faker->name();
+            $book->genre = $faker->text(15);
+            $book->price = rand(10, 100);
+            $book->cover = 'https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781442499577/random-9781442499577_hr.jpg';
+            $book->save();
+        }
     }
 }
